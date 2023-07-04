4356cc89cafab0bbc114061dfcf92bff88fe9e08
hiccupDragonMaster <hiccupseniordev@gmail.com>
2023-07-04 12:46:25 +0300

    bug fixed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressip().getIpInfoMiddleware);
-app.use(require('./utils/error'));
+// app.use(require('./utils/error'));
 
 app.use((req, res, next) => {
   // Website you wish to allow to connect
